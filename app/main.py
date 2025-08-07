b331b89d37c800235ba3bbe32d998a08cf3cd38b
Adwik Roshan <adwik.roshan@student.manchester.ac.uk>
2025-08-08 01:24:12 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,10 +3,19 @@ import os
 import zlib
 
 
+def cat_file_blob(fname):
+    with open(fname, "rb") as contentfile:
+        data = contentfile.read()
+        data_decompressed = zlib.decompress(data).decode("utf-8")
+        obj_storage_nullbyte = data_decompressed.find("\x00")
+        content = data_decompressed[obj_storage_nullbyte+1:].strip()
+        print(content, end="")
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
-    
+
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -15,16 +24,19 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
-    
+
     elif command == "cat-file":
-        file=sys.argv[3] # file name
-        filename=f".git/objects/{file[0:2]}/{file[2:]}" # based on the task info
-        with open(filename,"rb") as contentfile:
-            data=contentfile.read()
-            data_decompressed= zlib.decompress(data).decode("utf-8")
-            obj_storage_nullbyte=data_decompressed.find("\x00")
-            content= data_decompressed[obj_storage_nullbyte+1 : ].strip()
-            print(content, end= "")
+        file = sys.argv[3]  # file name
+        # based on the task info
+        filename = f".git/objects/{file[0:2]}/{file[2:]}"
+        cat_file_blob(filename)
+        # with open(filename, "rb") as contentfile:
+        #     data = contentfile.read()
+        #     data_decompressed = zlib.decompress(data).decode("utf-8")
+        #     obj_storage_nullbyte = data_decompressed.find("\x00")
+        #     content = data_decompressed[obj_storage_nullbyte+1:].strip()
+        #     print(content, end="")
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
