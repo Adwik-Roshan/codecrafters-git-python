6f807b747c957e8895d9b5a5e6fd2e3e9a8529fa
Adwik Roshan <adwik.roshan@student.manchester.ac.uk>
2025-08-07 22:33:47 +0530

    First draft submission for stage 2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -14,6 +15,15 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+    elif command == "cat-file":
+        file=sys.argv[3] # file name
+        filename=f".git/objects.{file[0:2]}/{file[2:]}"
+        with open(filename,"r") as contentfile:
+            data=contentfile.read()
+            data_decompressed= zlib.decompress(data)
+            object_storage_nullbyte=data_decompressed.find("\0")
+            content= data_decompressed[object_storage_nullbyte+1 : ]
+            sys.stdout.write(content)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
