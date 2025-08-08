a076722d26530d0ca6759f4ee1ce3a5afa1629a3
Adwik Roshan <adwik.roshan@student.manchester.ac.uk>
2025-08-08 14:45:04 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 
 def cat_file_blob(fname):
@@ -30,12 +31,31 @@ def main():
         # based on the task info
         filename = f".git/objects/{file[0:2]}/{file[2:]}"
         cat_file_blob(filename)
+        
         # with open(filename, "rb") as contentfile:
         #     data = contentfile.read()
         #     data_decompressed = zlib.decompress(data).decode("utf-8")
         #     obj_storage_nullbyte = data_decompressed.find("\x00")
         #     content = data_decompressed[obj_storage_nullbyte+1:].strip()
         #     print(content, end="")
+
+    elif command == "hash-object":
+        with open(sys.argv[3], "rb") as f:
+            filedata = f.read()
+        # filepath = f".git/objects/{filedata[0:2]}/{filedata[2:]}"
+        header= f"blob {len(filedata)}\x00".encode("utf-8")
+        # file = 'blob' + b' ' + str(len(filedata)).encode() + b'\x00' + filedata
+        file = header + filedata
+        hashed_file=hashlib.sha1(file).hexdigest()
+        print(hashed_file)
+        hash_dir=os.path.join(".git/objects",hashed_file[0:2])
+        os.mkdir(hash_dir)
+        # If we also need to write
+        if sys.argv[2]=='-w':
+            with open(os.path.join(hash_dir,hashed_file[2:]), "wb") as fp:
+                    fp.write(zlib.compress(file))
+            # hash=hashlib.sha1(res).hexdigest()
+            # print(hash)
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
